ee66fa83a4b32231deb0119324e9546f5895042c
Yuandi <c14210013@john.petra.ac.id>
2024-07-02 09:19:46 +0700

    copas latihan 1

---
+++ database/migrations/2024_07_01_063507_master_barang.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("master_barang", function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->string("nama_barang");
+            $table->longText("img_url");
+            $table->integer('qty');
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('master_barang');
+    }
+};
