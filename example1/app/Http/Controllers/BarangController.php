b2dee3ac5c090a782d7c6656da45777fc96b2472
danielchristianto08 <94582957+danielchristianto08@users.noreply.github.com>
2024-07-03 10:39:53 +0700

    barang

---
+++ example1/app/Http/Controllers/BarangController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Barang;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BarangController extends Controller
+{
+    public function get_barang(){
+        $search = request("s");
+        if($search == ""){
+            $data = Barang::with('satuan')->paginate(10);
+        }
+        else{
+            $data = Barang::where('nama_barang','LIKE','%',$search,'%')->with('satuan')->paginate(10);
+        }
+        return response($data,Response::HTTP_OK);
+    }
+
+    public function detail_barang($id){
+        $data = Barang::where('id',$id)->with('satuan')->first();
+        $result = [
+            'message' => 'Success',
+            'error' => 'false',
+        ];
+        return response($data,Response::HTTP_OK);
+    }
+}
