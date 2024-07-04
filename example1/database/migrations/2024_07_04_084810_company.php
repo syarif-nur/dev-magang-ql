00bc59c83304e047a3384c338f757179657253b8
danielchristianto08 <94582957+danielchristianto08@users.noreply.github.com>
2024-07-04 19:46:37 +0700

    revisi

---
+++ example1/database/migrations/2024_07_04_084810_company.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('supplier_id');
+            $table->string('company_name');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('postal_code');
+            $table->string('country');
+            $table->string('phone_number');
+            $table->string('website')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
