79dec2a49fde3af65b9465989795436b534fd4e4
lalahjr <najlahhajar12@gmail.com>
2024-07-04 16:33:32 +0700

    upload

---
+++ database/migrations/2024_07_04_091103_company.php
@@ -0,0 +1,37 @@
+<?php
+
+use Faker\Provider\ar_EG\Company;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('supplier_id');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('postal_code');
+            $table->string('country');
+            $table->string('phone_number');
+            $table->string('website');
+            $table->timestamps();
+
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company');
+    }
+};
