2d3e15f5cea6c89e369d123b8fa29f4bef1adf8a
lalahjr <najlahhajar12@gmail.com>
2024-07-06 14:39:48 +0700

    coba

--- database/migrations/2024_07_04_091103_company.php
+++ database/migrations/2024_07_04_091103_company.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('company', function (Blueprint $table) {
             $table->id();
+            // $table->string('name');
             $table->bigInteger('supplier_id');
             $table->string('address');
             $table->string('city');
