1ed0605538365567c69b3db426d3341da42e29cd
kiranashaloom <kiranashaloom29@gmail.com>
2024-07-09 15:44:34 +0700

    upload

--- database/migrations/2024_07_04_083931_supplier.php
+++ database/migrations/2024_07_04_083931_supplier.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('supplier', function (Blueprint $table) {
+        Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email');
-            $table->string('phone')->default(1);
+            $table->string('phone')->default(1); // Default value harus string
             $table->timestamps();
         });
     }
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::drop('supplier');
+        Schema::dropIfExists('suppliers'); // Menggunakan dropIfExists untuk reverse migration
     }
 };
--- database/migrations/2024_07_04_091535_supplier_address.php
+++ database/migrations/2024_07_04_091535_supplier_address.php
@@ -11,23 +11,28 @@ return new class extends Migration
      */
     public function up(): void
     {
+         if (!Schema::hasTable('supplier_address')) {
         Schema::create('supplier_address', function (Blueprint $table) {
             $table->id();
-            $table->string('supplier_id');
+            $table->bigInteger('supplier_id')->unsigned();
             $table->string('address');
             $table->string('city');
             $table->string('state');
             $table->string('zipcode');
             $table->string('country');
             $table->timestamps();
+
+            // Menambahkan foreign key constraint
+            $table->foreign('supplier_id')->references('id')->on('suppliers')->onDelete('cascade');
         });
     }
+}
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::drop('supplier_address');
+        Schema::dropIfExists('supplier_address');
     }
 };
